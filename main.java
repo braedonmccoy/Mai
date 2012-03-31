c98493d2aec2d85a8104e5faaf109dd3f1e5d8e0
Braedon McCoy <braedon.b.mccoy@gmail.com>
2012-03-31 00:59:07 -0400

    Dialogue finished

--- main.java
+++ main.java
@@ -12,16 +12,16 @@ public class main{
 	public static List<String> filtered = new ArrayList<String>(); // Common items from above two
 	public static List<String> full = new ArrayList<String>(); // Full list of entries in text
 	public static List<String> actions = new ArrayList<String>(); // Actions in txt.
-	public static List<String> todo = new ArrayList<String>(); // Actions in txt.
+	public static List<String> todo = new ArrayList<String>(); // Final commands to execute.
+	public static List<String> dialogue = new ArrayList<String>(); // Dialogue in txt.
+	public static List<String> response = new ArrayList<String>(); // Responses to dialogue.
 	public static int numCommands = 0;
 
 	public static void main(String[] args){
-		Scanner scan = new Scanner(System.in);
-		String text = "";
-		ArrayList arr = new ArrayList();
-		ArrayList action = new ArrayList();
-		String finalCommand;
 		String result;
+		boolean status;
+
+		print("Hello and welcome. I am MAI. How can I help you?\n");
 
 		result = getCommands("../command_list.txt");
 		if(result != ""){
@@ -31,23 +31,14 @@ public class main{
 		if(result != ""){
 			print(result);
 		}
-		print("MAI: Please enter what you want me to do: ");
-		text = scan.nextLine();
-		print("MAI: Working...");
-
-		//print("ArrayList: Commands");
-		//printAL(commands);
-		input = split(text); // input is an ArrayList that contains the input.
-		//print("ArrayList: ");
-		//printAL(input);
-		filtered = check(); // action is the commands found in the input.
-		//print("ArrayList: Filtered");
-		//printAL(actions);
-		if(filtered.size() == 0){
-			print("MAI: I'm not sure what you are trying to say...");
-		}else{
-			process(); // get associated actions from txt and runs them.
+		result = getDialogue("../dialogue_list.txt");
+		if(result != ""){
+			print(result);
 		}
+
+		do{
+			status = run();
+		}while(status == true);
 	}
 
 	public static void print(String text){
@@ -61,6 +52,22 @@ public class main{
 			System.out.println(al.get(i));
 		}
 		return;
+	}
+
+	public static String formatInput(String input){
+		char letter;
+		int ascii;
+		String formatted = "";
+
+		input = input.toLowerCase();
+		for(int i = 0; i< input.length(); i++){
+			letter = input.charAt(i);
+			ascii = letter;
+			if(((ascii >= 97) && (ascii <= 122)) || (ascii == 32)){
+				formatted += letter;
+			}
+		}
+		return(formatted);
 	}
 
 	public static List split(String orig){
@@ -162,22 +169,72 @@ public class main{
 		}
 	}
 
+	public static String getDialogue(String location){
+		String line = "";
+		try{
+				FileReader r = new FileReader(location);
+				if (!(r.ready())){
+
+				}
+					BufferedReader reader = new BufferedReader(r);
+				if(!reader.ready()){
+					print("ERROR: Cannot create Reader");
+					return("ERROR");
+				}else{
+					//print("Reader is ready. Getting Actions.");
+				}
+			while((line = reader.readLine()) != null){
+				if(line != ""){
+					dialogue.add(line);
+				}else{
+					return("ERROR: Action not found!");
+				}
+			}
+			return("");
+		} catch(IOException e){
+			return("ERROR JavaIOException e:" + e.getMessage());
+		}
+	}
+
+	public static boolean run(){
+		Scanner scan = new Scanner(System.in);
+		String text = "";
+
+		System.out.print("User: ");
+		text = scan.nextLine();
+		if(text.contains("exit")){
+			print("Goodbye...");
+			return(false);
+		}
+		text = formatInput(text);
+		//print(text);
+
+		input = split(text); // input is an ArrayList that contains the input.
+		filtered = check(); // action is the commands found in the input.
+		if(filtered.size() != 0){
+			process(); // get associated actions from txt and runs them.
+		}else{
+			response = checkDia();
+			if(response.size()  == 0){
+				print("MAI: I'm not sure what you are trying to say...");
+			}else{
+				process();
+			}
+		}
+		return(true);
+	}
+
 	public static ArrayList check(){ // Need to add backup check to check dialogue.
 		ArrayList common = new ArrayList(commands);
 		int numCommon = 0;
 		common.retainAll(input);
-		/*
-		for(Iterator<String> i = input.iterator(); i.hasNext();){	//Iterates through the ArrayList
-			String item = i.next();
-			print("Searched For: " + item);
-			if(commands.contains(item)){	//Checks for commonalities and adds them to Common
-				common.add(item);
-				print("Found: " + item);
-				numCommon++;
-			}
+		return(common);
+	}
 
-		}
-		*/
+	public static ArrayList checkDia(){ // Backup check to check for dialogue.
+		ArrayList common = new ArrayList(dialogue);
+		int numCommon = 0;
+		common.retainAll(input);
 		return(common);
 	}
 
@@ -186,37 +243,50 @@ public class main{
 		String act;
 		String tmp;
 		String str;
+		int index;
 
 		if(filtered.size() > 1){
 			todo.add(actions.get(commands.indexOf((String)filtered.get(0)))); // gets the action associated with the command
-			print(todo.get(0));
+			//print(todo.get(0));
 			for(i = 0; i < filtered.size(); i++){
 				if(!(todo.contains(actions.get(commands.indexOf((String)filtered.get(i)))))){
 					todo.add(actions.get(commands.indexOf((String)filtered.get(i))));
 					//print("" + i);
 				}
-				print(todo.get(i));
+				//print(todo.get(i));
 			}
 
-		}else{
+		}else if(filtered.size() == 1){
 			todo.add(actions.get(commands.indexOf((String)filtered.get(0))));
-		}
-				try{
+		}else{
+			if(dialogue.size() != 0){
+				index = dialogue.indexOf(response.get(0));
+				index++;
+				print("MAI: " + dialogue.get(index));
+				return;
+			}else{
+				print("MAI: I'm not sure what you are trying to say...");
+				return;
+			}
 
-					for(i = 1; i < todo.size(); i++){
-						if(todo.get(i) != null){
-							str = todo.get(0); // get the base command
-							tmp = todo.get(i); // get the argument
-							str += tmp; // add the argument to the end of the first command
-							todo.set(0, str); // set the first item to the full command
-							//print(todo.get(0));
-						}
+		}
+			try{
+
+				for(i = 1; i < todo.size(); i++){
+					if(todo.get(i) != null){
+						str = todo.get(0); // get the base command
+						tmp = todo.get(i); // get the argument
+						str += tmp; // add the argument to the end of the first command
+						todo.set(0, str); // set the first item to the full command
+						//print(todo.get(0));
 					}
-					Runtime rt = Runtime.getRuntime();
-					Process pr = rt.exec("cmd /c " + todo.get(0));
-				}catch(IOException e){
-					print("ERROR WITH RUNTIME");
 				}
+				Runtime rt = Runtime.getRuntime();
+				Process pr = rt.exec("cmd /c " + todo.get(0));
+			}catch(IOException e){
+				print("Sir, there was a problem: " + e.getMessage());
+			}
+		print("MAI: Yes sir.");
 		return;
 	}
 }
